4962c6316ccfa9dfd26f130a80ba1bb9949ea67c
Jay Allen <cooker98910@gmail.com>
2021-11-23 15:54:52 +0300

    create

---
+++ massage-app/resources/views/frontend/footer.blade.php
@@ -0,0 +1,144 @@
+<footer class="priority">
+    <div class="scrollUp">
+      <a href="#scrollTopPoint" class="pageLink">
+        <i class="scrollUpIcon">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1"
+            x="0px" y="0px" width="512px" height="512px" viewBox="0 0 284.929 284.929"
+            style="enable-background:new 0 0 284.929 284.929;" xml:space="preserve">
+            <path
+              d="M282.082,195.285L149.028,62.24c-1.901-1.903-4.088-2.856-6.562-2.856s-4.665,0.953-6.567,2.856L2.856,195.285   C0.95,197.191,0,199.378,0,201.853c0,2.474,0.953,4.664,2.856,6.566l14.272,14.271c1.903,1.903,4.093,2.854,6.567,2.854   c2.474,0,4.664-0.951,6.567-2.854l112.204-112.202l112.208,112.209c1.902,1.903,4.093,2.848,6.563,2.848   c2.478,0,4.668-0.951,6.57-2.848l14.274-14.277c1.902-1.902,2.847-4.093,2.847-6.566   C284.929,199.378,283.984,197.188,282.082,195.285z"
+              fill="#FFFFFF" />
+          </svg>
+        </i>
+      </a>
+    </div>
+    <div class="footerCont cont clearfix">
+      <div class="footerTop">
+        <div class="footerNav">
+          <ul>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="index.html">
+                HOME
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="system.html">
+                SYSTEM
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="schedule.html">
+                SCHEDULE
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="therapist.html">
+                THERAPIST
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="reservation.html">
+                RESERVATION
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="access.html">
+                ACCESS
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="recruit.html">
+                RECRUIT
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="policy.html">
+                POLICY
+              </a>
+            </li>
+            <li>
+              <a target="_blank" alt="Twitter" href="https://twitter.com/@bazu_ca4288">
+                <i class="fab fa-twitter" aria-hidden="true"></i>TWITTER<br>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" alt="LINE" href="https://line.me/ti/p/NiWB4ZPazB">
+                <i class="fab fa-line" aria-hidden="true"></i>LINE<br>
+              </a>
+            </li>
+            <li class="display-inline-block">
+              <a data-turbolinks="false" href="link.html">
+                <i class="fas fa-external-link-alt"></i>外部リンク
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="footerBottom clearfix">
+        <div class="footerLogo">
+          <a href="index.html" class="logoLink">
+            <img class="logo" alt="ロゴ"
+              src="assets/customer/logo-4235d13b1fef2284d93a5e03151f13ea181a9ded27ba70d02f063d9107b68974.png" />
+            <span>池袋 メンズエステ バズーカ</span>
+          </a>
+        </div>
+        <div class="footerText">
+          <p>池袋メンズエステ バズーカは、女性セラピストのクオリティや、bazu-ca独自のマッサージ法も導入し、全てのお客様に満足頂けるよう細部までこだわり運営しております。</p>
+        </div>
+        <div class="footerInfo displayNoneLess740">
+          <a onclick="gtag(&quot;event&quot;, &quot;tel&quot;, { &quot;event_category&quot;: &quot;cv&quot;, &quot;event_label&quot;: &quot;telclick&quot;, &quot;value&quot;: 1 });"
+            alt="09017004288" href="tel:09017004288">
+            <p>
+              <span class="icon"><img class=""
+                  src="assets/customer/telWhite-8bf33ed90821f4e9a39733e89e75d753d526aedfef79f9a01b94f48c702810f6.png" /></span>
+              09017004288
+              <span class="icon"><img class=""
+                  src="assets/customer/clockWhite-a8c9bd05254301f2f1a2859fb9089a0dfccdba9f76acc50de26c827c9f2c14ab.png" /></span>
+              10:00~05:00
+            </p>
+          </a>
+        </div>
+      </div>
+      <div class="footerCopyright">
+        <p>© 池袋のメンズエステ bazu-ca（バズーカ）</p>
+      </div>
+      <div class="footerProvided">
+        <a href="https://3days.tech" target="_blank">Provided by 3Day'sメンズエステ ホームページ制作</a>
+      </div>
+    </div>
+    <div class="footerFixedBtn displayNoneMore1023">
+      <ul>
+        <li>
+          <a onclick="gtag(&quot;event&quot;, &quot;tel&quot;, { &quot;event_category&quot;: &quot;cv&quot;, &quot;event_label&quot;: &quot;telclick&quot;, &quot;value&quot;: 1 });"
+            alt="09017004288" href="tel:09017004288">
+            <i class="fas fa-mobile-alt"></i><br>
+            お電話
+          </a>
+        </li>
+        <li>
+          <a alt="webで予約" data-turbolinks="false" href="reservation.html">
+            <i class="fas fa-edit"></i><br>
+            WEB
+          </a>
+        </li>
+        <li>
+          <a target="_blank" alt="LINE" href="https://line.me/ti/p/NiWB4ZPazB">
+            <i class="fab fa-line"></i><br>
+            LINE
+          </a>
+        </li>
+        <li>
+          <a href="#scrollTopPoint" class="pageLink">
+            <i class="fas fa-angle-double-up"></i><br>
+            トップへ
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+  <!-- ======== /footer ======== -->
+
+
+</body>
+
+</html>
